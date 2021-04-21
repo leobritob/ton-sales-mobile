1846642a1cecb3ad14b8ba6b45ae4b31e7eb8ca3
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 10:01:39 -0300

    feat: configure theme

---
+++ src/themes/theme.ts
@@ -0,0 +1,22 @@
+const breakpoints: any = ['576px', '768px', '992px', '1200px']
+
+breakpoints.sm = breakpoints[0]
+breakpoints.md = breakpoints[1]
+breakpoints.lg = breakpoints[2]
+breakpoints.xl = breakpoints[3]
+
+export const theme = {
+  breakpoints,
+  colors: {
+    primary: '#00c700',
+  },
+  radii: {
+    borderRadius: 5,
+  },
+}
+
+export type ThemeType = typeof theme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
