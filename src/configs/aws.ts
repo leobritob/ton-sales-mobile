d4f154833cbcb8f8aeeab3d44b407ab9fb37c0c3
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 14:51:59 -0300

    feat(amplify): change texts

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,11 +1,8 @@
-import Constants from 'expo-constants'
-
 export const amplifyConfigure = {
-  Auth: {
-    mandatorySignIn: true,
-    identityPoolId: Constants.manifest.extra.REACT_APP_AWS_IDENTITY_POOL_ID,
-    // userPoolId: Constants.manifest.extra.REACT_APP_USER_POOL_ID,
-    region: Constants.manifest.extra.REACT_APP_AWS_REGION,
-    userPoolWebClientId: Constants.manifest.extra.REACT_APP_AWS_USER_POOL_CLIENT_ID,
-  },
+  aws_project_region: 'us-east-1',
+  aws_cognito_identity_pool_id: 'us-east-1:9f114120-cb25-4853-bcd6-eae9839292aa',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_aoRM5BVic',
+  aws_user_pools_web_client_id: 'k4uealr96ouet9rp2va1ti8i0',
+  oauth: {},
 }
