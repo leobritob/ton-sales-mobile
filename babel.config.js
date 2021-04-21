d4f6109d03a2213713e3b69611e30c45501540b8
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 10:00:55 -0300

    feat: configure repository

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './src/components',
+            configs: './src/configs',
+            hooks: './src/hooks',
+            navigations: './src/navigations',
+            providers: './src/providers',
+            screens: './src/screens',
+            services: './src/services',
+            theme: './src/themes',
+          },
+        },
+      ],
+    ],
+  }
+}
