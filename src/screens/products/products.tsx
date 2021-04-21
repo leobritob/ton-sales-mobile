d5a784f04ca4c71e4f3b1759d01855f8b66ffaf4
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 16:46:50 -0300

    feat(products): improve screen

--- src/screens/products/products.tsx
+++ src/screens/products/products.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { View, SafeAreaView, StatusBar, Text } from 'react-native'
+import { View, StatusBar } from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack'
 
 import { RootStackParamList } from '../../navigations'
+import { Button, Column, Input, Products, Row, ShoppingCartSvg } from '../../components'
 
-export const ProductsScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Home'>) => {
+export const ProductsScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Products'>) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <SafeAreaView>
-        <StatusBar barStyle="default" />
-      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+
+      <Row width="100%" p="10px 20px" backgroundColor="primary" justifyContent="space-between">
+        <Input flex={1} placeholder="Buscar produto" backgroundColor="#fff" border="none" />
+
+        <Column flex={1 / 4} alignItems="flex-end">
+          <Button variant="transparent">
+            <ShoppingCartSvg count={5} />
+          </Button>
+        </Column>
+      </Row>
+
+      <Products />
     </View>
   )
 }
